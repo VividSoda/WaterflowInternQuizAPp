f8aafde3b025ad32d51581397e7c2926c9d025b9
VividSoda <prajwal.thapa.986@gmail.com>
2024-01-29 17:24:06 +0545

    quiz options made ynamic

--- lib/screens/quiz.dart
+++ lib/screens/quiz.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:waterflow_intern/api/quiz_api.dart';
 import 'package:waterflow_intern/models/quiz.dart';
 import 'package:waterflow_intern/widgets/answer_tile.dart';
 
@@ -22,11 +23,14 @@ class _QuizScreenState extends State<QuizScreen> {
   late Timer _quizTimer;
   int _currentQuestionIndex = 0;
   int _selectedOption = -1;
+  List<Quiz> quizQuestions = [];
+  bool _loading = true;
 
   @override
   void initState() {
     super.initState();
     _startTimer();
+    _getQuizList();
   }
 
   @override
@@ -49,149 +53,168 @@ class _QuizScreenState extends State<QuizScreen> {
     });
   }
 
+  void _getQuizList() async {
+    List<Quiz> questions = await QuizApi.getQuizQuestions();
+    setState(() {
+      quizQuestions = questions;
+      _loading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final scrHeight = MediaQuery.of(context).size.height;
     final scrWeidth = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: const Color(0xFFf1f2f6),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Icon(
-                            Icons.arrow_back_ios_new,
-                            color: Color(0xFF334c4c),
-                          ),
-                        ),
-                        const Text(
-                          "Previous",
-                          style: TextStyle(
-                            color: Color(0xFF334c4c),
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          width: scrWeidth * 0.15,
-                        ),
-                        const Text(
-                          "7/10",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: scrHeight * 0.125,
-                    ),
-                    Stack(
-                      clipBehavior: Clip.none,
-                      children: [
-                        Container(
-                          width: double.infinity,
-                          height: scrHeight * 0.3,
-                          padding: const EdgeInsets.all(20),
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(25),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey
-                                    .withOpacity(0.5), // Shadow color
-                                spreadRadius: 5, // Spread radius
-                                blurRadius: 7, // Blur radius
-                                offset: const Offset(
-                                    0, 3), // Offset to bottom-right
+      body: _loading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : SafeArea(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: SingleChildScrollView(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Icon(
+                                  Icons.arrow_back_ios_new,
+                                  color: Color(0xFF334c4c),
+                                ),
+                              ),
+                              const Text(
+                                "Previous",
+                                style: TextStyle(
+                                  color: Color(0xFF334c4c),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(
+                                width: scrWeidth * 0.15,
+                              ),
+                              const Text(
+                                "7/10",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
+                                ),
                               ),
                             ],
                           ),
-                          child: const Text(
-                            "Question",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
+                          SizedBox(
+                            height: scrHeight * 0.125,
                           ),
-                        ),
-                        Positioned(
-                          top: -scrWeidth * 0.125,
-                          left: scrWeidth * 0.35,
-                          child: CircularPercentIndicator(
-                            radius: scrWeidth * 0.1,
-                            lineWidth: 10.0,
-                            percent: 0.8,
-                            center: const Text(
-                              '30',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 27,
-                                color: Color(0xFF0e4b4a),
+                          Stack(
+                            clipBehavior: Clip.none,
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                height: scrHeight * 0.3,
+                                padding: const EdgeInsets.all(20),
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(25),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey
+                                          .withOpacity(0.5), // Shadow color
+                                      spreadRadius: 5, // Spread radius
+                                      blurRadius: 7, // Blur radius
+                                      offset: const Offset(
+                                          0, 3), // Offset to bottom-right
+                                    ),
+                                  ],
+                                ),
+                                child: Text(
+                                  quizQuestions[0].question,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
                               ),
-                            ),
-                            backgroundColor: const Color(0xFFb7d6ce),
-                            progressColor: const Color(0xFF0e4b4a),
+                              Positioned(
+                                top: -scrWeidth * 0.125,
+                                left: scrWeidth * 0.35,
+                                child: CircularPercentIndicator(
+                                  radius: scrWeidth * 0.1,
+                                  lineWidth: 10.0,
+                                  percent: 0.8,
+                                  center: const Text(
+                                    '30',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 27,
+                                      color: Color(0xFF0e4b4a),
+                                    ),
+                                  ),
+                                  backgroundColor: const Color(0xFFb7d6ce),
+                                  progressColor: const Color(0xFF0e4b4a),
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
+                          SizedBox(
+                            height: scrHeight * 0.07,
+                          ),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: quizQuestions[0].answerOptions.length,
+                            itemBuilder: (context, index) {
+                              return AnswerTile(
+                                value: index,
+                                selectAnswer: _selectedOption > 0
+                                    ? null
+                                    : (value) {
+                                        setState(() {
+                                          _selectedOption = value ?? -1;
+                                        });
+                                      },
+                                groutValue: _selectedOption,
+                                answerOption:
+                                    quizQuestions[0].answerOptions[index],
+                                isCorrect:
+                                    quizQuestions[0].answerOptions[index] ==
+                                        quizQuestions[0].correctAnswer,
+                              );
+                            },
+                          ),
+                        ],
+                      ),
                     ),
-                    SizedBox(
-                      height: scrHeight * 0.07,
-                    ),
-                    ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: 4,
-                      itemBuilder: (context, index) {
-                        return AnswerTile(
-                          option: index,
-                          selectAnswer: (value) {
-                            setState(() {
-                              _selectedOption = value ?? -1;
-                            });
-                          },
-                          groutValue: _selectedOption,
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              margin: const EdgeInsets.all(20),
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF0a4f4c),
-                  foregroundColor: Colors.white,
-                  padding: const EdgeInsets.symmetric(vertical: 15),
-                ),
-                child: const Text(
-                  "Next",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
                   ),
-                ),
+                  Container(
+                    width: double.infinity,
+                    margin: const EdgeInsets.all(20),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF0a4f4c),
+                        foregroundColor: Colors.white,
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                      ),
+                      child: const Text(
+                        "Next",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
--- lib/widgets/answer_tile.dart
+++ lib/widgets/answer_tile.dart
@@ -3,44 +3,56 @@ import 'package:flutter/material.dart';
 class AnswerTile extends StatelessWidget {
   const AnswerTile({
     super.key,
-    required this.option,
+    required this.value,
     required this.selectAnswer,
     required this.groutValue,
-  });
+    required this.answerOption,
+    required this.isCorrect,
+  }) : isSelected = value == groutValue;
 
-  final int option;
-  final void Function(int? selectedOption) selectAnswer;
+  final int value;
+  final void Function(int? selectedOption)? selectAnswer;
   final int groutValue;
+  final String answerOption;
+  final bool isSelected;
+  final bool isCorrect;
 
   @override
   Widget build(BuildContext context) {
-    final scrHeight = MediaQuery.of(context).size.height;
     return Column(
       children: [
         Container(
           width: double.infinity,
-          height: scrHeight * 0.07,
-          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+          // height: scrHeight * 0.07,
+          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
           alignment: Alignment.center,
           decoration: BoxDecoration(
-            color: Colors.white,
+            color: !isSelected
+                ? Colors.white
+                : isCorrect
+                    ? const Color(0xFFb5d6cd)
+                    : Colors.red,
             borderRadius: BorderRadius.circular(25),
           ),
           child: Row(
             children: [
-              const Text(
-                "Option",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+              Expanded(
+                child: Text(
+                  answerOption,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
                 ),
               ),
-              const Spacer(),
-              Radio(
-                activeColor: Colors.black,
-                value: option,
-                groupValue: groutValue,
-                onChanged: selectAnswer,
+              Align(
+                alignment: Alignment.centerRight,
+                child: Radio(
+                  activeColor: Colors.black,
+                  value: value,
+                  groupValue: groutValue,
+                  onChanged: selectAnswer,
+                ),
               ),
             ],
           ),
