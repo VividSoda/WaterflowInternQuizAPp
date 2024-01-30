d0dd8e748be1a4d24b09f55f9be1f46f59461ae1
VividSoda <prajwal.thapa.986@gmail.com>
2024-01-30 11:10:27 +0545

    added validation

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -55,7 +55,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void _startQuiz() async {
-    // _validateName();
+    _validateName();
     final bool isValid = _errorText == null;
     if (!isValid) {
       return;
